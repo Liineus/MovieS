27d3d981303149d5f466e437e980d6a4f126e480
Liineus <37506390+Liineus@users.noreply.github.com>
2018-03-18 18:08:21 +0100

    Add files via upload

---
+++ js/jcarousel.responsive.js
@@ -0,0 +1,34 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = 200;
+                } else if (width >= 350) {
+                    width = 200;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        
+    });
+})(jQuery);
